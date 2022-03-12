25ed9646f81fba8b08beffbc31161de8e98ff0bc
Nikit099 <nechit1337@gmail.com>
2022-03-12 15:16:39 +0300

    тестирую код

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import './styles.css'
-import {Menu} from './core/menu' // добавил я
+import { Menu } from './core/menu' // добавил я
 
 const menu = new Menu('.menu') // добавил я
-menu.open()
+
+document.body.addEventListener('click', e => menu.open(e))
--- src/core/menu.js
+++ src/core/menu.js
@@ -1,32 +1,33 @@
 export class Menu {
-  constructor(selector) {
-    this.menuElement = document.querySelector(selector)
+    constructor(selector) {
+        this.menuElement = document.querySelector(selector)
 
-    document.body.addEventListener('click', event => {
-      if (event.target.offsetParent !== this.menuElement) {
-        this.close()
-      }
-    })
-  }
+        document.body.addEventListener('click', event => {
+            if (event.target.offsetParent !== this.menuElement) {
+                this.close()
+            }
+        })
+    }
 
-  open() {
-    document.body.addEventListener('contextmenu', (event) => {
-      event.preventDefault()
-      this.menuElement.textContent = 'Тестовое название' // потом удалить
-      this.menuElement.style.top = `${event.y}px`
-      this.menuElement.style.left = `${event.x}px`
-      this.menuElement.classList.add('open')
-    })
-    // throw new Error(`"open" method should be implemented in Menu"`)
-  }
+    open(event) {
 
-  close() {
-    this.menuElement.classList.remove('open')
-    // throw new Error(`"close" method should be implemented in Menu"`)
-  }
 
-  add() {
-    this.menuElement.append(newElement)
-    // throw new Error(`"add" method should be implemented in Menu"`)
-  }
+        console.log('sd');
+        // this.menuElement.textContent = 'Тестовое название' // потом удалить
+        this.menuElement.style.top = `${event.y}px`
+        this.menuElement.style.left = `${event.x}px`
+        this.menuElement.classList.add('open')
+
+        // throw new Error(`sda"open" method should be implemented in Menu"`)
+    }
+
+    close() {
+        this.menuElement.classList.remove('open')
+            // throw new Error(`"close" method should be implemented in Menu"`)
+    }
+
+    add() {
+        this.menuElement.append(newElement)
+            // throw new Error(`"add" method should be implemented in Menu"`)
+    }
 }
