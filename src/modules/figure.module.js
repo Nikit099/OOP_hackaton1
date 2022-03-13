6e9ee09eeb835af5eb353d70adf6d0b2e7edc120
ibakhtir <ibakhtir@gmail.com>
2022-03-13 12:17:45 +0200

    fix bugs

--- src/modules/figure.module.js
+++ src/modules/figure.module.js
@@ -15,8 +15,8 @@ export class FigureModule extends Module {
     const figure = document.createElement('div')
     figure.className = 'figure'
     figure.style.position = 'absolute'
-    figure.style.left = `${random(1, 1600)}px`
-    figure.style.top = `${random(1, 800)}px`
+    figure.style.left = `${random(1, document.body.clientWidth - 400)}px`
+    figure.style.top = `${random(1, document.body.clientHeight - 300)}px`
     figure.style.width = random(50, 400) + 'px'
     figure.style.height = random(50, 300) + 'px'
     figure.style.borderRadius = `20px`
